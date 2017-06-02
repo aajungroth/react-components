86e813f8795cc3e8f13afa2d7be4b06ca1227e9f
Alex Jungroth <aajungroth@ucdavis.edu>
2017-06-02 19:05:19 +0000

    Renders Mustard and Watermelon components to the screen

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var Mustard = () => (
+  <li>"mustard"</li>
+);
+
+var Watermelon = () => (
+  <li>"watermelon"</li>
+);
+
+var GroceryList = () => (
+  <ul>
+    <Mustard />
+    <Watermelon />
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
