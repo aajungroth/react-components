854cc94e0e02397f6401955e489e08f8a56a2fcd
Alex Jungroth <aajungroth@ucdavis.edu>
2017-06-02 19:19:02 +0000

    Refactored GroceryList to use GroceryListItem

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ var Watermelon = () => (
   <li>"watermelon"</li>
 );
 
-var GroceryList = () => (
+var GroceryListItem = (props) => (
   <ul>
-    <Mustard />
-    <Watermelon />
+    <li>{props.groceryItems[0]}</li>
+    <li>{props.groceryItems[1]}</li>
+    <li>{props.groceryItems[2]}</li>
   </ul>
+);
+
+var GroceryList = () => (
+  <GroceryListItem groceryItems =
+    {['watermelon', 'mustard', 'lentils']}/>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
